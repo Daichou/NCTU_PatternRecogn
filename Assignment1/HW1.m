4108f3e31c18517cc12e36ccac886cf5e20ff6be
Daichou <tommy0705c@gmail.com>
2018-10-16 10:55:05 +0800

    Assignment 1: P3 P4 finished

--- Assignment1/HW1.m
+++ Assignment1/HW1.m
@@ -4,8 +4,10 @@ x = [-3:.1:9]; % range
 norm = normpdf(x,3,2); % range, avg, stddev
 figure;
 plot(x,norm)
+title('P11-d Gaussian function ')
 
 % HW1 p2
+figure
 mu = [1 2];
 Sigma = [1 0; 0 1]; % covariance
 x1 = -2:.1:4; x2 = -1:.1:5; % (min, step , max)
@@ -16,3 +18,21 @@ surf(x1,x2,F);
 caxis([min(F(:))-.5*range(F(:)),max(F(:))]);
 axis([-2 4 -1 5 0 .2])
 xlabel('x1'); ylabel('x2'); zlabel('Probability Density');
+title('P2  2-d Gaussian function')
+
+% HW1 p3
+rng default;
+r= normrnd(0,1,10000,1) % mu, sigma, x = 100000 y = 1 (2d array)
+figure
+histogram(r)
+title('P3 1-d Gaussian random data')
+
+% HW1 p4
+mu = [1 2];
+sigma = [3 0; 0 4];
+rng default  % For reproducibility
+R = mvnrnd(mu,sigma,10000);
+figure
+plot(R(:,1),R(:,2),'.')
+figure
+hist3([R(:,1),R(:,2)],'CDataMode','auto','FaceColor','interp','Ctrs', {-8:0.2:10 -10:0.2:14} )
