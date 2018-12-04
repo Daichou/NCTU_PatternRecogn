5d377376dda1c007f114cd22ef929f1d86bf7890
Daichou <tommy0705c@gmail.com>
2018-12-04 19:25:31 +0800

    Assignment2-a: ABIJK p1 partial success iter 120 120 120 200000 eta 0.9

--- Assignment2-a/MLP_P1_ABIJK.m
+++ Assignment2-a/MLP_P1_ABIJK.m
@@ -16,11 +16,11 @@ data(98:98+96,4) = 1;
 % A = 2+1 % B = 5+1; % I=3+1;% J=3+1;% K=2;
 nvectors=N*2;
 ninpdim_with_bias=3;
-neuron_hid_layerJ=80;
+neuron_hid_layerJ=120;
 neuron_hid_layerJ_with_bias=neuron_hid_layerJ+1;
-neuron_hid_layerI=80;
+neuron_hid_layerI=120;
 neuron_hid_layerI_with_bias=neuron_hid_layerI+1;
-neuron_hid_layerB=80;
+neuron_hid_layerB=120;
 neuron_hid_layerB_with_bias=neuron_hid_layerB+1;
 noutdim=2;
 
@@ -55,8 +55,8 @@ dk = zeros(noutdim,1);        % desired output
 
 Lowerlimit=0.01;
 itermax=20000;
-eta=0.8;            % (n -> eta -> learning rate)
-beta=0.2;           % momentum term
+eta=0.9;            % (n -> eta -> learning rate)
+beta=0.1;           % momentum term
  
 iter=0;
 error_avg=10;
