c90d5200d5b2c8d44cff25275983403512c7539d
Daichou <tommy0705c@gmail.com>
2018-12-10 17:01:21 +0800

    Assignment2-a:sigmoid seperate

---
+++ Assignment2-a/src/sigmoid/train_ABIJK_net.m
@@ -0,0 +1,156 @@
+
+function [wkj,wji,wib,wba,error_r,ite] = train_ABIJK_net(data,eta,beta,layer,input,output,itermax,Lowerlimit)
+    nvectors=length(data);
+    ninpdim_with_bias=input+1;
+    neuron_hid_layerJ=layer(1);
+    neuron_hid_layerJ_with_bias=neuron_hid_layerJ+1;
+    neuron_hid_layerI=layer(2);
+    neuron_hid_layerI_with_bias=neuron_hid_layerI+1;
+    neuron_hid_layerB=layer(3);
+    neuron_hid_layerB_with_bias=neuron_hid_layerB+1;
+    noutdim=output;
+
+    %initialize
+    wkj = randn(noutdim,neuron_hid_layerI_with_bias);
+    wji = randn(neuron_hid_layerJ_with_bias,neuron_hid_layerI_with_bias);
+    wib = randn(neuron_hid_layerI_with_bias,neuron_hid_layerB_with_bias);
+    wba = randn(neuron_hid_layerB_with_bias,ninpdim_with_bias);
+    wkj_tmp = zeros(size(wkj));
+    wji_tmp = zeros(size(wji));
+    wib_tmp = zeros(size(wib));
+    wba_tmp = zeros(size(wba));
+    olddelwkj=zeros(noutdim , neuron_hid_layerJ_with_bias); % weight of Wkj (J -> K)
+    olddelwji=zeros(neuron_hid_layerJ_with_bias , neuron_hid_layerI_with_bias);   % weight of Wji (I -> J)
+    olddelwib=zeros(neuron_hid_layerB_with_bias , neuron_hid_layerI_with_bias);   % weight of Wji (B -> I)
+    olddelwba=zeros(neuron_hid_layerB_with_bias , ninpdim_with_bias);   % weight of Wji (A -> B)
+    oa = zeros(ninpdim_with_bias,1);
+    oa(ninpdim_with_bias) = 1;
+
+    sb = zeros(neuron_hid_layerB_with_bias,1);
+    ob = zeros(neuron_hid_layerB_with_bias,1);
+    ob(neuron_hid_layerB_with_bias) = 1;       % output of data
+
+    si = zeros(ninpdim_with_bias,1);       % input of hidden layer i
+    oi = zeros(neuron_hid_layerJ_with_bias,1);
+    oi(neuron_hid_layerI_with_bias) = 1;    % output of hidden layer i
+
+    sj = zeros(neuron_hid_layerI_with_bias,1);      % input of hidden layer j
+    oj = zeros(neuron_hid_layerJ_with_bias,1);
+    oj(neuron_hid_layerJ_with_bias) = 1;    % output of hidden layer j
+
+    sk = zeros(neuron_hid_layerJ_with_bias,1);        % input of output layer k
+    ok = zeros(noutdim,1);        % net output
+    dk = zeros(noutdim,1);        % desired output
+
+    iter=0;
+    error_avg=10;
+
+    % internal variables
+    deltak = zeros(1,noutdim);
+    deltaj = zeros(1,neuron_hid_layerJ_with_bias);
+    deltai = zeros(1,neuron_hid_layerI_with_bias);
+    deltab = zeros(1,neuron_hid_layerB_with_bias);
+    sumback = zeros(1,max(neuron_hid_layerJ_with_bias, max(neuron_hid_layerI_with_bias,neuron_hid_layerB_with_bias)));
+     
+    while (error_avg > Lowerlimit) && (iter<itermax)
+        iter=iter+1;
+        error=0;
+     
+    % Forward Computation:
+        for ivector=1:nvectors
+            oa=[data(ivector,1) data(ivector,2) 1]';
+            dk=[data(ivector,3) data(ivector,4)]';
+
+            for j=1:neuron_hid_layerB
+                sb(j)=wba(j,:)*oa;
+                ob(j)=1/(1+exp(-sb(j)));    % sigmoid
+            end
+            ob(neuron_hid_layerB_with_bias)=1.0;
+
+            for j=1:neuron_hid_layerI
+                si(j)=wib(j,:)*ob;
+                oi(j)=1/(1+exp(-si(j)));    % sigmoid
+            end
+            oi(neuron_hid_layerI_with_bias)=1.0;
+
+            for j=1:neuron_hid_layerJ
+                sj(j)=wji(j,:)*oi;
+                oj(j)=1/(1+exp(-sj(j)));    % sigmoid
+            end
+            oj(neuron_hid_layerJ_with_bias)=1.0;
+     
+            for k=1:noutdim
+                sk(k)=wkj(k,:)*oj;
+                ok(k)=1/(1+exp(-sk(k)));    % signmoid
+            end
+            
+            %error=error+ (dk-ok)' *(dk-ok)/2;
+            error=error+sum(abs(dk-ok)); % abs is absolute each element
+            
+    % Backward learning:
+     
+             for k=1:noutdim
+                deltak(k)=(dk(k)-ok(k))*ok(k)*(1.0-ok(k)); % gradient term
+             end
+     
+             for j=1:neuron_hid_layerJ_with_bias
+                for k=1:noutdim
+                   wkj_tmp(k,j)=wkj(k,j)+eta*deltak(k)*oj(j)+beta*olddelwkj(k,j);
+                   olddelwkj(k,j)=eta*deltak(k)*oj(j)+beta*olddelwkj(k,j);
+                end
+             end
+     
+             for j=1:neuron_hid_layerJ
+                sumback(j)=0.0;
+                for k=1:noutdim
+                   sumback(j)=sumback(j)+deltak(k)*wkj(k,j);
+                end
+                deltaj(j)=oj(j)*(1.0-oj(j))*sumback(j);
+             end
+
+             for j=1:neuron_hid_layerI_with_bias
+                for k=1:neuron_hid_layerJ
+                   wji_tmp(k,j)=wji(k,j)+eta*deltaj(k)*oi(j)+beta*olddelwji(k,j);
+                   olddelwji(k,j)=eta*deltaj(k)*oi(j)+beta*olddelwji(k,j);
+                end
+             end
+
+             for j=1:neuron_hid_layerI
+                sumback(j)=0.0;
+                for k=1:neuron_hid_layerJ_with_bias
+                   sumback(j)=sumback(j)+deltaj(k)*wji(k,j);
+                end
+                deltai(j)=oi(j)*(1.0-oi(j))*sumback(j);
+             end
+     
+             for j=1:neuron_hid_layerB_with_bias
+                for k=1:neuron_hid_layerI
+                   wbi_tmp(k,j)=wib(k,j)+eta*deltai(k)*ob(j)+beta*olddelwib(k,j);
+                   olddelwib(k,j)=eta*deltai(k)*ob(j)+beta*olddelwib(k,j);
+                end
+             end
+
+             for j=1:neuron_hid_layerB
+                sumback(j)=0.0;
+                for k=1:neuron_hid_layerI_with_bias
+                   sumback(j)=sumback(j)+deltai(k)*wib(k,j);
+                end
+                deltab(j)=ob(j)*(1.0-ob(j))*sumback(j);
+             end
+
+             for i=1:ninpdim_with_bias
+                for j=1:neuron_hid_layerB
+                   wba(j,i)=wba(j,i)+eta*deltab(j)*oa(i)+beta*olddelwba(j,i);
+                   olddelwba(j,i)=eta*deltab(j)*oa(i)+beta*olddelwba(j,i);
+                end
+             end
+
+             wkj = wkj_tmp;
+
+        end
+     
+        ite(iter)=iter;
+        error_avg=error/nvectors;
+        error_r(iter)=error_avg;
+    end
+end
