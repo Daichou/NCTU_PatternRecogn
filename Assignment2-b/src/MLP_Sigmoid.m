1d14816230cb423cd8915498e0ebbe93b683b629
Daichou <tommy0705c@gmail.com>
2018-12-16 14:57:53 +0800

    Assignment2-b: template

--- Assignment2-b/src/MLP_Sigmoid.m
+++ Assignment2-b/src/MLP_Sigmoid.m
@@ -39,7 +39,7 @@ file_text = sprintf('Sigmoid_ABIJK_%dX%dX%d_iter_%d_eta_%f_beta_%f',n_input,laye
 
 fig_error = figure(1);
 hold on;
-
+set(fig_error, 'Position', get(0, 'Screensize'));
 plot(ite, error_r);
 title(title_text);
 xlabel('iteration');
@@ -49,6 +49,7 @@ saveas(fig_error,strcat(file_text,'_error.fig'));
 
 fig_time = figure(2);
 hold on;
+set(fig_time, 'Position', get(0, 'Screensize'));
 
 plot(time_r, error_r);
 title(title_text);
@@ -61,8 +62,41 @@ for ix=1:1:10000
     oi = single([x_test_list(ix,1:784) 1].');
     ok = FeedFoward_IJK(wji,wkj,oi,784,10,layer);
     [M,I] = max(ok);
-    result_r(ix) = I;
+    result_r(ix) = I-1;
 end
 
-Confu = confusionmat(y_test_list, result_r);
-confusionchart(Confu);
+
+fig_train = figure(3)                                          % plot images
+set(fig_train, 'Position', get(0, 'Screensize'));
+colormap(gray)                                  % set to grayscale
+for i = 1:100
+    subplot(10,10,i)
+    digit = reshape(x_train(:,i), 28,28)';    % row = 28 x 28 image
+    imagesc(digit)                              % show the image
+end
+saveas(fig_train,strcat(file_text,'_train.jpg'));
+saveas(fig_train,strcat(file_text,'_train.fig'));
+
+fig_test = figure(4)                       % plot images
+set(fig_test, 'Position', get(0, 'Screensize'));
+colormap(gray)                                  % set to grayscale
+for i = 1:100
+    subplot(10,10,i)
+    digit = reshape(x_test(:,i), 28,28)';    % row = 28 x 28 image
+    imagesc(digit)                              % show the image
+    if y_test(i,1) == result_r(i)
+        title_str = sprintf('desire:%d,pred:%d,True',y_test(i,1),result_r(i));
+    else
+        title_str = sprintf('desire:%d,pred:%d,True',y_test(i,1),result_r(i));
+    end
+    title(title_str)                    % show the label
+end
+
+saveas(fig_train,strcat(file_text,'_test.jpg'));
+saveas(fig_train,strcat(file_text,'_test.fig'));
+fig_confu = figure(5)
+set(fig_confu, 'Position', get(0, 'Screensize'));
+Confu = confusionmat(single(y_test), single(result_r.'));
+confusionchart(int32(Confu));
+saveas(fig_train,strcat(file_text,'_conf.jpg'));
+saveas(fig_train,strcat(file_text,'_conf.fig'));
